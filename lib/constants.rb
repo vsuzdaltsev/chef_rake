f427df462a03ea28f522dc178c1e5a3af305201d
Vsevolod Suzdaltsev <vsuzdaltsev@example.com>
2019-03-15 13:44:26 +0300

    added default node_name equla to node

--- lib/constants.rb
+++ lib/constants.rb
@@ -4,6 +4,7 @@ module Rake
       IDENTITY_FILE ||= ->(args) { File.expand_path("../.vagrant/machines/#{args[:node]}/virtualbox/private_key") }.freeze
       SSH_USER      ||= 'vagrant'.freeze
       ENV_DIR       ||= 'environments/'.freeze
+      NODE_NAME     ||= ->(args) { args[:node] }.freeze
     end
   end
 end
